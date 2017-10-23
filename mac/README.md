9dcba378fa014c2eeb6e7700b6080a4c30b0f2bd
Nelson Tran <nutty@ntt.io>
2017-10-22 18:53:40 -0700

    Add macOS configuration

---
+++ mac/README.md
@@ -0,0 +1,7 @@
+The `install.sh` script automatically configures some macOS settings:
+
+- Dock configuration (`autohide` = true, `tilesize` = 48, `largesize` = 64).
+
+- Keyboard settings (DAS = 8, ARR = 1) — I like Tetris. 凸
+
+- Enable dark mode for the Dock and menu bar.
---
+++ mac/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd -P)
+cd "brew/"
+
+source "$DOTFILES_ROOT/install/utils.sh"
+
+install_macos_settings() {
+	if [ $(uname) = "Darwin" ]
+	then
+		star "Installing macOS settings"
+
+		# Dock settings.
+		success "Setting up dock configuration"
+		defaults write com.apple.dock autohide -bool TRUE
+		defaults write com.apple.dock tilesize -int 48
+		defaults write com.apple.dock largesize -float 64
+		killall Dock
+
+		# Set the DAS and ARR values for the keyboard.
+		success "Cranking up DAS and ARR settings to the max!"
+		defaults write -g InitialKeyRepeat -int 8
+		defaults write -g KeyRepeat -int 1
+
+		# Turn on dark mode.
+		success "Turning off the lights (dark mode!)"
+		dark-mode on
+	fi
+}
+
+install_macos_settings
